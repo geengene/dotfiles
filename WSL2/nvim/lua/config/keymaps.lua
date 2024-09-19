04101dc04a874945eedd06afd21dcfc5716fffac
gene <gqkuah2505@gmail.com>
2024-09-19 10:38:54 +0800

    all esc sequences also save the file

--- WSL2/nvim/lua/config/keymaps.lua
+++ WSL2/nvim/lua/config/keymaps.lua
@@ -1,7 +1,7 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
-vim.keymap.set("i", "kj", "<ESC>>", { noremap = true, silent = false })
+vim.keymap.set("i", "kj", "<ESC>:w<Return>", { noremap = true, silent = false })
 vim.keymap.set("n", "L", "g$", { noremap = true, silent = false })
 vim.keymap.set("n", "H", "g^", { noremap = true, silent = false })
 vim.keymap.set("v", "L", "g$", { noremap = true, silent = false })
