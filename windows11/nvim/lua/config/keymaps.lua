61d6019bbc18b598eec735a180485f0423eaca01
gene <gqkuah2505@gmail.com>
2024-10-06 08:40:34 +0800

    changed tab behaviour on nvim

--- windows11/nvim/lua/config/keymaps.lua
+++ windows11/nvim/lua/config/keymaps.lua
@@ -1,12 +1,14 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
-vim.keymap.set("i", "kj", "<ESC>", { noremap = true, silent = false })
+vim.keymap.set("i", "kj", "<ESC>", { noremap = true, silent = true })
 vim.keymap.set("n", "L", "g$", { noremap = true, silent = false })
 vim.keymap.set("n", "H", "g^", { noremap = true, silent = false })
 vim.keymap.set("v", "L", "g$", { noremap = true, silent = false })
 vim.keymap.set("v", "H", "g^", { noremap = true, silent = false })
 vim.keymap.set("o", "H", "^", { noremap = true, silent = false })
 vim.keymap.set("o", "L", "$", { noremap = true, silent = false })
+vim.keymap.set("n", "<Tab>", ":bnext<Return>", { noremap = true, silent = true })
+vim.keymap.set("n", "<S-Tab>", ":bprevious<Return>", { noremap = true, silent = true })
 
 vim.keymap.set("v", "q", "<ESC>", { noremap = true, silent = true })
