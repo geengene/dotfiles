bf0ef0e0c85529253b222ed946d573455d0e38bb
gene <gqkuah2505@gmail.com>
2024-10-13 21:16:19 +0800

    vscode ctrl-a keybind to select all

--- windows11/nvim/init.lua
+++ windows11/nvim/init.lua
@@ -37,7 +37,7 @@ if vim.g.vscode then
   -- stylua: ignore
   keys = {
     { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
-    { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+    { "S", mode = { "n" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
     { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
     { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
     { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
@@ -132,6 +132,7 @@ if vim.g.vscode then
   vim.api.nvim_set_keymap("n", "<S-Tab>", ":Tabprev<CR>", { noremap = true, silent = true })
 
   -- miscellaneous
+  vim.api.nvim_set_keymap("n", "<C-a>", "gg<S-v>G", { noremap = true, silent = true })
   vim.opt.clipboard = "unnamedplus"
 else
   -- bootstrap lazy.nvim, LazyVim and your plugins
